4fe55e3f477a5e7d8d98e62caf9f0cc248ba5a60
Tyler Seward <tylerseward2341@gmail>
2021-10-29 20:35:47 -0500

    Write a program that solves the 100 doors problem

--- 100_Doors/Program.cs
+++ 100_Doors/Program.cs
@@ -10,6 +10,34 @@ namespace _100_Doors
     {
         static void Main(string[] args)
         {
+            bool[] doors = new bool[100];
+
+            for (int i = 0; i < doors.Length; i++)
+            {
+                doors[i] = false;
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < doors.Length; j++)
+                {
+                    if ((j+1) % (i+1) == 0)
+                    {
+                        doors[j] = !doors[j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < doors.Length; i++)
+            {
+                if (doors[i])
+                    Console.WriteLine(String.Format("Door {0}: Open", i + 1));
+
+                else
+                    Console.WriteLine(String.Format("Door {0}: Closed", i + 1));
+            }
+
+            Console.ReadKey();
         }
     }
 }
